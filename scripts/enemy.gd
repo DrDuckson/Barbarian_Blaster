310947e807ee56d22652c639d3e2c2602db1342e
DrDuckson <gorealex95@web.de>
2024-07-17 23:11:59 +0200

    Added logic to free enemy and deactivate _process

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,6 +1,13 @@
 extends PathFollow3D
 
 @export var speed: float = 2.5
+@onready var enemy: PathFollow3D = $"."
+@onready var base = get_tree().get_first_node_in_group("grBase")
 
 func _process(delta: float) -> void:
 	progress += delta * speed
+	if progress_ratio == 1.0:
+		base.take_damage()
+		set_process(false)
+		enemy.queue_free()
+		
