fec2fb5cd73ae16c8e50f78d473bc75055c9ef44
DrDuckson <gorealex95@web.de>
2024-08-16 23:13:46 +0200

    Added methods for turning turrets

--- scripts/TurretManager.gd
+++ scripts/TurretManager.gd
@@ -1,14 +1,16 @@
 extends Node3D
 
 @export var turret: PackedScene
+@export var enemy_path: Path3D
+
 var new_turret
- #= preload("res://scenes/turret.tscn")
+#= preload("res://scenes/turret.tscn")
 
 func build_turret(turret_position: Vector3) -> void:
 	new_turret = turret.instantiate()
 	add_child(new_turret)
 	new_turret.global_position = turret_position
+	new_turret.enemy_path = enemy_path
 	
 #func destroy_turret(turret_position: Vector3) -> void:
 	#new_turret.destroy
-	#pass	
--- scripts/turret.gd
+++ scripts/turret.gd
@@ -1,10 +1,31 @@
 extends Node3D
 
 @export var projectile: PackedScene
-
-func _ready() -> void:
-	var shot = projectile.instantiate()
-	add_child(shot)
+var enemy_path: Path3D
+var target: PathFollow3D
+@onready var turret_barrel: MeshInstance3D = $TurretBase/TurretTop/TurretBarrel
 
 func destroy():
 	queue_free()
+	
+func _physics_process(delta: float) -> void:
+	target = find_best_target()
+	if target:
+		look_at(target.global_position, Vector3.UP, true)
+
+func _on_timer_timeout() -> void:
+	if target:
+		var shot = projectile.instantiate()
+		add_child(shot)
+		shot.global_position = turret_barrel.global_position
+		shot.direction = global_transform.basis.z
+	
+func find_best_target() -> PathFollow3D:
+	var best_target = null
+	var best_progress = 0
+	for enemy in enemy_path.get_children():
+		if enemy is PathFollow3D:
+			if enemy.progress > best_progress:
+				best_target = enemy
+				best_progress = enemy.progress
+	return best_target
